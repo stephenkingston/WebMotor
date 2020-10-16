bb3c868ad8d5dbe92a913090bfb82196b26b4cdf
Stephen Kingston <stephen.psk@gmail.com>
2020-10-16 15:22:05 +0530

    Added a basic stepper motor with slider control of rotation.

--- docs/js/3d.js
+++ docs/js/3d.js
@@ -1,6 +1,5 @@
 import * as THREE from "./3js-lib/three.module.js";
 import {OrbitControls} from "./3js-lib/OrbitControls.js";
-import {FBXLoader} from "./3js-lib/FBXLoader.js";
 import {GLTFLoader} from "./3js-lib/GLTFLoader.js";
 
 const update = function ()
@@ -25,6 +24,9 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(25, window.innerWidth/window.innerHeight, 0.1, 500);
 const renderer = new THREE.WebGLRenderer();
 
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
 renderer.gammaOutput = true;
 renderer.gammaFactor = 2.2;
 
@@ -45,23 +47,50 @@ loader.load("https://raw.githubusercontent.com/stephenkingston/WebMotor/master/d
                     if (node instanceof THREE.Mesh)
                     {
                         node.castShadow = true;
+                        node.receiveShadow = true;
                     }
                 })
-                scene.add(model.scene)
+                model.scene.name = "motorBody";
+                model.scene.translate
+                scene.add(model.scene);
+});
+
+/* GitHub link: https://raw.githubusercontent.com/stephenkingston/WebMotor/master/docs/models/shaft.gltf */
+/* Local link to 3D file - ../docs/models/shaft.gltf */
+
+loader.load("https://raw.githubusercontent.com/stephenkingston/WebMotor/master/docs/models/shaft.gltf", (model) => {
+    model.scene.traverse((node) =>{
+        if (node instanceof THREE.Mesh)
+        {
+            node.castShadow = true;
+            node.receiveShadow = true;
+        }
+    })
+    model.scene.name = "shaft";
+    scene.add(model.scene);
 });
 
 /* Lighting */
-const light = new THREE.AmbientLight(0xFFFFFF, 0.3);
-const dirLight = new THREE.PointLight(0xFFFFFF, 1.4, 10, 0.1);
-dirLight.position.set(5, 6, 3);
-dirLight.castShadow = true;
+const light = new THREE.AmbientLight(0xFFFFFF, 0.9);
+const dirLight1 = new THREE.DirectionalLight(0xFFFFFF, 1);
+const dirLight2 = new THREE.DirectionalLight(0xFFFFFF, 1);
+dirLight1.position.set(5, 6, 5);
+dirLight2.position.set(5, -6, -5);
+dirLight1.castShadow = true;
+
+/* Axes Helper */
+let axesHelper = new THREE.AxesHelper( 1 );
+scene.add(axesHelper);
+
+let helper = new THREE.BoxHelper(scene.getObjectByName("shaft"));
+scene.add(helper);
 
 /* Add all the things */
 scene.add(light);
-scene.add(dirLight);
+scene.add(dirLight1);
+scene.add(dirLight2);
 camera.position.set(0.156, 0.209, -0.263);
 
 export {scene, camera, renderer}
 
 GameLoop();
-
--- docs/js/util.js
+++ docs/js/util.js
@@ -1,4 +1,5 @@
-import {camera, renderer} from "./3d.js";
+import {scene, camera, renderer} from "./3d.js";
+import * as THREE from "./3js-lib/three.module.js";
 
 window.addEventListener('resize', () => {
     let width = window.innerWidth;
@@ -7,3 +8,11 @@ window.addEventListener('resize', () => {
     camera.aspect = width/height;
     camera.updateProjectionMatrix();
 });
+
+let rotSlider = document.getElementById("rotationSlider");
+
+rotSlider.addEventListener('input', () => {
+    let shaft = scene.getObjectByName("shaft");
+    shaft.rotation.z = rotSlider.value * (Math.PI / 180.0);
+});
+
